6bbf52cee7a4d3a987754e62ba957a565bc385de
salamander <salamander2977@gmail.com>
2026-04-03 17:58:48 +0300

    Added Anthropic module

---
+++ src/test/java/org/sideqquest/sidequestai/SidequestaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sideqquest.sidequestai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidequestaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
